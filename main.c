4298b9bd42d32b226d44557fe9fe605bfffe9f94
Beegala <beegalayuvraj@gmail.com>
2012-04-14 11:45:34 +0530

    Give a personalized hello

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include<string.h>
 int main(int argc, char **argv)
 {
-	printf("Hello world!\n");
+char name[255];
+printf("enter your name:");
+fgets(name,255,stdin);
+printf("length=%d\n",strlen(name));
+name[strlen(name)-1]='\0';
+	printf("Hello %s!\n",name);
 	return 0;
 }
