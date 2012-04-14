c613e1dbd8a9c06f6b9350f039f80246390194ad
Beegala <beegalayuvraj@gmail.com>
2012-04-14 11:51:57 +0530

    prompt your last name as well

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include<string.h>
 int main(int argc, char **argv)
 {
-char name[255];
-printf("enter your name:");
-fgets(name,255,stdin);
+char first[255],last[255];
+printf("enter your first name:");
+fgets(first,255,stdin);
+first[strlen(first)-1]='\0';
+printf("plz enter ur last name");
+gets(last);
 printf("length=%d\n",strlen(name));
 name[strlen(name)-1]='\0';
-	printf("Hello %s!\n",name);
+	printf("Hello %s %s!\n",first,last);
 	return 0;
 }
